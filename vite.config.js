397e1918973e44ad11c2f9fc04817841924624f6
Adam Sayyed <adam.a.sayyed@gmail.com>
2026-06-17 21:44:41 -0400

    Build to docs/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/A1/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 })
 
